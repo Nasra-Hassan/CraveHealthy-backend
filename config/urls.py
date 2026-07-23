bac533f4b40f9009822d69e17046e437e6b5213a
Nasra <nasra.hassan@student.moringaschool.com>
2026-07-23 13:38:36 +0300

    created ingredient rest api endpoints

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/recipes/', include('recipes.urls')),
+    path('api/ingredients/', include('ingredients.urls')),
 ]
