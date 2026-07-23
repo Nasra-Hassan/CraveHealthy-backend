2add805c2b7ff0573065a7a249eaca4dcc752798
Nasra <nasra.hassan@student.moringaschool.com>
2026-07-23 12:59:38 +0300

    Created recipe REST API endpoints

---
+++ recipes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RecipeListCreateView, RecipeDetailView
+
+
+urlpatterns = [
+    path("", RecipeListCreateView.as_view(), name="recipe-list-create"),
+    path("<int:pk>/", RecipeDetailView.as_view(), name="recipe-detail"),
+]
